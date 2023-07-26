74a553ce8bcb946a82f7b9e5329ce0910a375a95
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2023-07-26 21:29:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='crawler',
     version='0.1',
     packages=find_packages(),
-    install_requires=[flair,duckduckgo_search,pandas,numpy,matplotlib,pytesseract,cv2,requests,bs4,re,PIL,fastdownload,time],
+    install_requires=['flair','duckduckgo_search','pandas','numpy','matplotlib','pytesseract','cv2','requests','bs4','re','PIL','fastdownload','time'],
     author='Harsh Jadhav',
     author_email='jadhavharshvardhan6301@gmail.com',
     description='A python package using OCR, NLP for goal of extracting and mining information through web searches *In development*',
